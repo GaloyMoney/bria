5a203af33382b399c5ecf50662d8263afb9b4326
bodymindarts <justin@galoy.io>
2022-11-19 08:41:39 -0600

    chore: shared pg settings with sqlx-ledger

--- tests/xpub.rs
+++ tests/xpub.rs
@@ -4,7 +4,7 @@ use bria::xpub::*;
 #[tokio::test]
 async fn test_xpub() -> anyhow::Result<()> {
     let pg_host = std::env::var("PG_HOST").unwrap_or("localhost".to_string());
-    let pg_con = format!("postgres://bria:bria@{pg_host}:5432/bria");
+    let pg_con = format!("postgres://user:password@{pg_host}:5432/pg");
     let pool = sqlx::PgPool::connect(&pg_con).await?;
 
     // let account_id = create_test_account(&pool).await?;
