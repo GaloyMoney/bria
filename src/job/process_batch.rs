335a9aa326cd79a9bb2cb51f53697c871c98c31f
Sandipan Dey <hey@sandipan.dev>
2023-01-16 12:24:46 +0530

    feat: process ledger entries of a batch

--- src/job/process_batch.rs
+++ src/job/process_batch.rs
@@ -1,9 +1,8 @@
-use crate::{
-    app::BlockchainConfig, batch_group::*, bdk::pg::Utxos, error::*, ledger::*, payout::*,
-    primitives::*, wallet::*,
-};
 use serde::{Deserialize, Serialize};
 use tracing::instrument;
+use uuid::Uuid;
+
+use crate::{app::BlockchainConfig, batch::*, error::*, ledger::*, primitives::*, wallet::*};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ProcessBatchData {
@@ -17,10 +16,43 @@ pub async fn execute(
     data: ProcessBatchData,
     blockchain_cfg: BlockchainConfig,
     ledger: Ledger,
+    wallets: Wallets,
+    batches: Batches,
 ) -> Result<ProcessBatchData, BriaError> {
     // load psbt
     // for each keychain
     // for each xpub
     // load signer
+
+    let Batch {
+        id,
+        bitcoin_tx_id,
+        batch_group_id,
+        wallet_summaries,
+    } = batches.find_by_id(data.batch_id).await?;
+
+    for (wallet_id, wallet_summary) in wallet_summaries.into_iter() {
+        let tx = pool.begin().await?;
+        let wallet = wallets.find_by_id(wallet_id).await?;
+
+        ledger
+            .create_batch(
+                tx,
+                CreateBatchParams {
+                    journal_id: wallet.journal_id,
+                    outgoing_ledger_account_id: wallet.ledger_account_ids.outgoing_id,
+                    at_rest_ledger_account_id: wallet.ledger_account_ids.at_rest_id,
+                    satoshis: wallet_summary.total_out_sats,
+                    external_id: Uuid::new_v4(), // TODO: Some hash of format!("{}-{}", bitcoin_tx_id, wallet_id)
+                    meta: CreateBatchMeta {
+                        batch_id: id,
+                        batch_group_id,
+                        bitcoin_tx_id,
+                    },
+                },
+            )
+            .await?;
+    }
+
     Ok(data)
 }
