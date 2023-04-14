50f58762508e6fb3536c9cc85ad06ebff32d936a
bodymindarts <justin@galoy.io>
2023-04-14 14:51:47 +0200

    ci: bump in charts

---
+++ ci/tasks/bump-image-digest.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+ set -eu
+
+ export digest=$(cat ./latest-image/digest)
+ export sidecar_digest=$(cat ./latest-provision-admin-sidecar-image/digest)
+ export ref=$(cat ./repo/.git/short_ref)
+ export app_version=$(cat version/version)
+
+ pushd charts-repo
+
+ yq -i e '.bria.image.digest = strenv(digest)' ./charts/${CHARTS_SUBDIR}/values.yaml
+ yq -i e '.bria.provisionAdminSidecarImage.digest = strenv(sidecar_digest)' ./charts/${CHARTS_SUBDIR}/values.yaml
+ yq -i e '.bria.image.git_ref = strenv(ref)' ./charts/${CHARTS_SUBDIR}/values.yaml
+ yq -i e '.appVersion = strenv(app_version)' ./charts/${CHARTS_SUBDIR}/Chart.yaml
+
+ if [[ -z $(git config --global user.email) ]]; then
+   git config --global user.email "bot@galoy.io"
+ fi
+ if [[ -z $(git config --global user.name) ]]; then
+   git config --global user.name "CI Bot"
+ fi
+
+ (
+   cd $(git rev-parse --show-toplevel)
+   git merge --no-edit ${BRANCH}
+   git add -A
+   git status
+   git commit -m "chore(${CHARTS_SUBDIR}): bump ${CHARTS_SUBDIR} image to '${digest}'"
+ )
---
+++ ci/tasks/open-charts-pr.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -eu
+
+export digest=$(cat ./latest-image/digest)
+
+pushd charts-repo
+
+ref=$(yq e '.bria.image.git_ref' charts/${CHARTS_SUBDIR}/values.yaml)
+git checkout ${BRANCH}
+old_ref=$(yq e '.bria.image.git_ref' charts/${CHARTS_SUBDIR}/values.yaml)
+
+cat <<EOF >> ../body.md
+# Bump ${CHARTS_SUBDIR} image
+
+The ${CHARTS_SUBDIR} image will be bumped to digest:
+\`\`\`
+${digest}
+\`\`\`
+
+Code diff contained in this image:
+
+https://github.com/GaloyMoney/bria/compare/${old_ref}...${ref}
+EOF
+
+gh pr close ${BOT_BRANCH} || true
+gh pr create \
+  --title "chore(deps) bump-bria-image-${ref}" \
+  --body-file ../body.md \
+  --base ${BRANCH} \
+  --head ${BOT_BRANCH} \
+  --label galoybot
