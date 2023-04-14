94a0d8cb06d3c70bb713adc66ae94f040efcda68
bodymindarts <justin@galoy.io>
2023-04-14 11:31:03 +0200

    ci: missing ci scripts

---
+++ ci/tasks/set-dev-version.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+VERSION="$(cat version/version)-dev"
+
+pushd repo
+
+sed -i'' "0,/version/{s/version.*/version = \"${VERSION}\"/}" Cargo.toml
+sed -i'' "/^name = \"bria/,/version/{s/version.*/version = \"${VERSION}\"/}" ./Cargo.lock
+
+if [[ -z $(git config --global user.email) ]]; then
+  git config --global user.email "bot@galoy.io"
+fi
+if [[ -z $(git config --global user.name) ]]; then
+  git config --global user.name "CI Bot"
+fi
+
+git status
+git add -A
+
+if [[ "$(git status -s -uno)" != ""  ]]; then
+  git commit -m "ci(dev): set version to ${VERSION}"
+fi
+
