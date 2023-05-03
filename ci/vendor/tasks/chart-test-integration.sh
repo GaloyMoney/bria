4a77b8d2ac5d4f3ccee5bf8688663d721eba645e
bodymindarts <justin@galoy.io>
2023-05-04 00:24:25 +0200

    ci: add docker compose pull to integration test

--- ci/vendor/tasks/chart-test-integration.sh
+++ ci/vendor/tasks/chart-test-integration.sh
@@ -40,6 +40,7 @@ rsync --delete --exclude target -avr -e "ssh -l ${DOCKER_HOST_USER} ${ADDITIONAL
 echo "Done!"
 
 docker compose down --remove-orphans --timeout 1
+docker compose pull
 
 ssh ${ADDITIONAL_SSH_OPTS} ${DOCKER_HOST_USER}@${DOCKER_HOST_IP} \
   "cd ${REPO_PATH}; docker compose -f docker-compose.yml up integration-tests"
