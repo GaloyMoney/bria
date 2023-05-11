513bab42341125c5f56e38e93aaa92e18bec8e55
Vaibhav Dixit <thevaibhavdixit@gmail.com>
2023-05-11 17:35:42 +0530

    feat: add serialize/deserialize support for SignerEncryptionConfig

--- src/xpub/signer_config.rs
+++ src/xpub/signer_config.rs
@@ -1,39 +1,66 @@
+use super::signing_client::*;
+use crate::error::*;
 use chacha20poly1305::{
     aead::{Aead, AeadCore, KeyInit, OsRng},
     ChaCha20Poly1305,
 };
 use serde::{Deserialize, Serialize};
 
-use super::signing_client::*;
-use crate::error::*;
+pub type EncryptionKey = chacha20poly1305::Key;
+pub(super) struct ConfigCyper(Vec<u8>);
+pub(super) struct Nonce(Vec<u8>);
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub struct SignerEncryptionConfig {
-    #[serde(skip)]
+    #[serde(with = "key_serialization")]
     pub key: Option<EncryptionKey>,
 }
 
 mod key_serialization {
     use super::EncryptionKey;
+    use serde::de::{Error, Unexpected};
+    use serde::{Deserialize, Deserializer, Serializer};
 
-    pub(super) fn deserialize<'de, D>(deserializer: D) -> Result<EncryptionKey, D::Error>
+    pub(super) fn deserialize<'de, D>(deserializer: D) -> Result<Option<EncryptionKey>, D::Error>
     where
-        D: serde::Deserializer<'de>,
+        D: Deserializer<'de>,
     {
-        unimplemented!()
+        let hex_string = String::deserialize(deserializer)?;
+        let key_vec = hex::decode(&hex_string).map_err(|_err| {
+            D::Error::invalid_value(Unexpected::Str(&hex_string), &"valid hex string")
+        })?;
+
+        let key_bytes = key_vec.as_slice();
+        if key_bytes.len() == 32 {
+            Ok(Some(*chacha20poly1305::Key::from_slice(key_bytes)))
+        } else {
+            Err(D::Error::invalid_length(key_bytes.len(), &"32-byte array"))
+        }
     }
 
-    pub(super) fn serialize<S>(key: &EncryptionKey, serializer: S) -> Result<S::Ok, S::Error>
+    pub(super) fn serialize<S>(
+        key: &Option<EncryptionKey>,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error>
     where
-        S: serde::Serializer,
+        S: Serializer,
     {
-        unimplemented!()
+        match key {
+            Some(key) => {
+                let key_bytes = key.as_slice();
+                let hex_string = hex::encode(key_bytes);
+                serializer.serialize_str(&hex_string)
+            }
+            None => serializer.serialize_none(),
+        }
     }
 }
 
-pub type EncryptionKey = chacha20poly1305::Key;
-pub(super) struct ConfigCyper(Vec<u8>);
-pub(super) struct Nonce(Vec<u8>);
+impl Default for SignerEncryptionConfig {
+    fn default() -> Self {
+        Self { key: None }
+    }
+}
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(tag = "type", rename_all = "snake_case")]
@@ -90,5 +117,16 @@ mod tests {
         let decrypted = SignerConfig::decrypt(&key, encrypted, nonce).expect("Failed to decrypt");
 
         assert_eq!(signer, decrypted);
+    }
+
+    #[test]
+    fn serialize_deserialize() {
+        let key = gen_encryption_key();
+        let signer_encryption_config = SignerEncryptionConfig { key: Some(key) };
+        let serialized = serde_json::to_string(&signer_encryption_config).unwrap();
+        let deserialized: SignerEncryptionConfig = serde_json::from_str(&serialized).unwrap();
+        assert!(deserialized.key.is_some());
+        assert_eq!(deserialized.key.unwrap(), key);
+        assert_eq!(signer_encryption_config, deserialized)
     }
 }
