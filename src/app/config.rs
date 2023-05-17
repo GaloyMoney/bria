6b65e2c58b839748a243a5ce3cf7d608440e8a18
Vaibhav <thevaibhavdixit@gmail.com>
2023-05-17 13:47:06 +0530

    fix: change bitcoin to mainnet for BlockchainConfig

--- src/app/config.rs
+++ src/app/config.rs
@@ -1,10 +1,10 @@
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize};
 
 use crate::{job::JobsConfig, primitives::bitcoin::Network, xpub::SignerEncryptionConfig};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct BlockchainConfig {
-    #[serde(default = "default_network")]
+    #[serde(default = "default_network", deserialize_with = "deserialize_network")]
     pub network: Network,
     #[serde(default = "default_electrum_url")]
     pub electrum_url: String,
@@ -35,4 +35,22 @@ pub struct AppConfig {
     pub jobs: JobsConfig,
     #[serde(default)]
     pub signer_encryption: SignerEncryptionConfig,
+}
+
+fn deserialize_network<'de, D>(deserializer: D) -> Result<Network, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+
+    match s.as_str() {
+        "mainnet" => Ok(Network::Bitcoin),
+        "testnet" => Ok(Network::Testnet),
+        "signet" => Ok(Network::Signet),
+        "regtest" => Ok(Network::Regtest),
+        _ => Err(serde::de::Error::unknown_variant(
+            &s,
+            &["mainnet", "testnet", "signet", "regtest"],
+        )),
+    }
 }
