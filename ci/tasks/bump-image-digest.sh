27ab3e26747a99f73f3a665e65466b597c14da46
Justin Carter <justin@galoy.io>
2023-06-03 02:43:12 +0200

    ci: remove sidecar image (#207)

--- ci/tasks/bump-image-digest.sh
+++ ci/tasks/bump-image-digest.sh
@@ -3,14 +3,12 @@
  set -eu
 
  export digest=$(cat ./latest-image/digest)
- export sidecar_digest=$(cat ./latest-provision-admin-sidecar-image/digest)
  export ref=$(cat ./repo/.git/short_ref)
  export app_version=$(cat version/version)
 
  pushd charts-repo
 
  yq -i e '.bria.image.digest = strenv(digest)' ./charts/${CHARTS_SUBDIR}/values.yaml
- yq -i e '.bria.provisionAdminSidecarImage.digest = strenv(sidecar_digest)' ./charts/${CHARTS_SUBDIR}/values.yaml
  yq -i e '.bria.image.git_ref = strenv(ref)' ./charts/${CHARTS_SUBDIR}/values.yaml
  yq -i e '.appVersion = strenv(app_version)' ./charts/${CHARTS_SUBDIR}/Chart.yaml
 
