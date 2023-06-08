c8084aa342029a64bc88b131f24940577ba26e1c
bodymindarts <justin@galoy.io>
2023-06-08 21:37:53 +0200

    test: increase max_connections in helpers::init_pool

--- tests/helpers.rs
+++ tests/helpers.rs
@@ -19,7 +19,10 @@ use rand::distributions::{Alphanumeric, DistString};
 pub async fn init_pool() -> anyhow::Result<sqlx::PgPool> {
     let pg_host = std::env::var("PG_HOST").unwrap_or("localhost".to_string());
     let pg_con = format!("postgres://user:password@{pg_host}:5432/pg");
-    let pool = sqlx::PgPool::connect(&pg_con).await?;
+    let pool = sqlx::postgres::PgPoolOptions::new()
+        .max_connections(20)
+        .connect(&pg_con)
+        .await?;
     Ok(pool)
 }
 
