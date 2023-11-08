e588fc1046da65f7f65c402c7b1e5e6bfa648a06
Kartik Shah <krtk.6160@gmail.com>
2023-11-08 17:59:48 +0530

    ci: new metadata scheme

--- ci/tasks/open-charts-pr.sh
+++ ci/tasks/open-charts-pr.sh
@@ -3,12 +3,15 @@
 set -eu
 
 export digest=$(cat ./latest-image/digest)
+export ref=$(cat ./repo/.git/short_ref)
 
 pushd charts-repo
 
-ref=$(yq e '.bria.image.git_ref' charts/${CHARTS_SUBDIR}/values.yaml)
 git checkout ${BRANCH}
-old_ref=$(yq e '.bria.image.git_ref' charts/${CHARTS_SUBDIR}/values.yaml)
+
+old_digest=$(yq e '.bria.image.digest' "./charts/${CHARTS_SUBDIR}/values.yaml")
+old_ref=$(grep "digest: \"${old_digest}\"" "./charts/${CHARTS_SUBDIR}/values.yaml" \
+  | sed -n 's/.*commit_ref=\([^;]*\);.*/\1/p' | tr -d ' \n')
 
 cat <<EOF >> ../body.md
 # Bump ${CHARTS_SUBDIR} image
@@ -20,14 +23,14 @@ ${digest}
 
 Code diff contained in this image:
 
-https://github.com/GaloyMoney/bria/compare/${old_ref}...${ref}
+https://github.com/GaloyMoney/${CHARTS_SUBDIR}/compare/${old_ref}...${ref}
 EOF
 
 export GH_TOKEN="$(ghtoken generate -b "${GH_APP_PRIVATE_KEY}" -i "${GH_APP_ID}" | jq -r '.token')"
 
 gh pr close ${BOT_BRANCH} || true
 gh pr create \
-  --title "chore(deps) bump-bria-image-${ref}" \
+  --title chore-bump-${CHARTS_SUBDIR}-image-${ref} \
   --body-file ../body.md \
   --base ${BRANCH} \
   --head ${BOT_BRANCH} \
