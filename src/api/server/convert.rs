99a101bfb91779319a4cdcc0a28e91e6fc4e9e51
Justin Carter <justin@galoy.io>
2024-03-25 11:21:35 +0100

    fix: payout external (#500)
    * chore: bump flake
    
    * fix: clippy
    
    * fix: expressive PayoutError:ExternalIdAlreadyExists

--- src/api/server/convert.rs
+++ src/api/server/convert.rs
@@ -645,6 +645,9 @@ impl From<ApplicationError> for tonic::Status {
             ApplicationError::PayoutError(PayoutError::PayoutIdNotFound(_)) => {
                 tonic::Status::not_found(err.to_string())
             }
+            ApplicationError::PayoutError(PayoutError::ExternalIdAlreadyExists) => {
+                tonic::Status::already_exists(err.to_string())
+            }
             ApplicationError::CouldNotParseIncomingMetadata(_) => {
                 tonic::Status::invalid_argument(err.to_string())
             }
