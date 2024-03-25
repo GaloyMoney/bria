84cd649e4b236c0a2b225c045ba19e986438ba41
bodymindarts <justin@galoy.io>
2024-03-25 11:19:10 +0100

    fix: expressive PayoutError:ExternalIdAlreadyExists

--- src/payout/error.rs
+++ src/payout/error.rs
@@ -3,7 +3,7 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 pub enum PayoutError {
     #[error("PayoutError - Sqlx: {0}")]
-    Sqlx(#[from] sqlx::Error),
+    Sqlx(sqlx::Error),
     #[error("PayoutError - EntityError: {0}")]
     EntityError(#[from] crate::entity::EntityError),
     #[error("PayoutError - Could not find payout with id: {0}")]
@@ -14,4 +14,19 @@ pub enum PayoutError {
     PayoutAlreadyCommitted,
     #[error("PayoutError - Payout is already cancelled")]
     PayoutAlreadyCancelled,
+    #[error("PayoutError - external_id already exists")]
+    ExternalIdAlreadyExists,
+}
+
+impl From<sqlx::Error> for PayoutError {
+    fn from(error: sqlx::Error) -> Self {
+        if let Some(err) = error.as_database_error() {
+            if let Some(constraint) = err.constraint() {
+                if constraint.contains("external_id") {
+                    return Self::ExternalIdAlreadyExists;
+                }
+            }
+        }
+        Self::Sqlx(error)
+    }
 }
