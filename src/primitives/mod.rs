d75a2757611614b24549a5de4f8899d713c09980
Lakshyyaa <99789174+Lakshyyaa@users.noreply.github.com>
2025-05-30 01:45:03 +0530

    fix: use entity_id macro from es_entity (#611)
    * fix: use entity_id macro from es_entity
    
    * chore: use es_entity directly

--- src/primitives/mod.rs
+++ src/primitives/mod.rs
@@ -21,13 +21,12 @@ impl From<AccountId> for LedgerJournalId {
         Self::from(uuid::Uuid::from(id))
     }
 }
-crate::entity_id! { ProfileId }
+es_entity::entity_id! { ProfileId, PayoutQueueId }
 crate::entity_id! { ProfileApiKeyId }
 crate::entity_id! { SigningSessionId }
 crate::entity_id! { KeychainId }
 crate::entity_id! { SignerId }
 crate::entity_id! { WalletId }
-crate::entity_id! { PayoutQueueId }
 crate::entity_id! { PayoutId }
 
 impl From<PayoutId> for LedgerTransactionId {
