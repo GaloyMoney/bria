2acc283f7f3034b05227e436573100ac22ec3d10
Kartik Shah <krtk.6160@gmail.com>
2025-06-17 06:43:43 +0530

    ci: check code

--- ci/vendor/tasks/check-code.sh
+++ ci/vendor/tasks/check-code.sh
@@ -1,10 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 #! Auto synced from Shared CI Resources repository
 #! Don't change this file, instead change it in github.com/GaloyMoney/concourse-shared
 
-set -eu
+set -euo pipefail
 
-pushd repo
+nix -L develop ./repo -c sh -exc '
+set -euo pipefail
 
-nix develop -c make check-code
+source pipeline-tasks/ci/vendor/tasks/helpers.sh
+
+cd repo
+make check-code
+'
