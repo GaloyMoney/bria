04c8b3be1c11923cd84b1e0e933177a497d7bd2d
Lakshyyaa <lakshh113@gmail.com>
2025-09-19 00:18:31 +0530

    chore: run sqlx prepare

--- src/payout/repo.rs
+++ src/payout/repo.rs
@@ -2,7 +2,7 @@ use es_entity::*;
 use sqlx::{Pool, Postgres};
 use tracing::instrument;
 
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 use super::{entity::*, error::*, unbatched::*};
 use crate::primitives::*;
